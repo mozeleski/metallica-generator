6a59951652dd53b435e15f6ca6de0bb17d66e7d5
mozeleski <tylermoz9@gmail.com>
2022-11-29 17:16:36 -0500

    fixed payload

--- generator.py
+++ generator.py
@@ -1,21 +1,22 @@
 import imp
-import json
 import re
-import names
+import json
 import time
+import names
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from twocaptcha import TwoCaptcha
+from selenium.webdriver.common.by import By
 
 with open('config.json') as config:
     script_config = json.load(config)
 
 captcha_config = {
-            'server':  '2captcha.com',
-            'apiKey':           '8fd1a9973973bf0093d3d1540f3ff0f3',
-            'softId':            123,
-            'callback':         'https://your.site/result-receiver',
+            'server': '2captcha.com',
+            'apiKey': '8fd1a9973973bf0093d3d1540f3ff0f3',
+            'softId':  123,
+            'callback': 'https://your.site/result-receiver',
             'defaultTimeout':    120,
             'recaptchaTimeout':  600,
             'pollingInterval':   10,
@@ -48,11 +49,22 @@ class Generator:
         )
 
         response = result['captchaId']
-        time.sleep(3)
+        time.sleep(5)
         captcha_result = self.s.get(f'http://2captcha.com/res.php?key={api_key}&action=get&id={response}&json=1')
         request = captcha_result.json()['request']
+        max_retry = 0
+        while request == 'CAPTCHA_NOT_READY':
+            print('retrying...')
+            if max_retry < 5:
+                time.sleep(3)
+                captcha_result = self.s.get(f'http://2captcha.com/res.php?key={api_key}&action=get&id={response}&json=1')
+                request = captcha_result.json()['request']
+                max_retry += 1
+            else:
+                break
+
         print(f'captcha response... {captcha_result.status_code}')
-        return captcha_result.json()['request']
+        return request
 
     def get_cookie(self):
         driver = webdriver.Firefox(executable_path='//Users//ty//Downloads//geckodriver\ 2 ')
@@ -93,6 +105,9 @@ class Generator:
         _ga_A = cookies_dict['_ga_8MT9WZCNB1']
         _ga_B = cookies_dict['_ga_QKEQR920KZ']
 
+        # registration_form = driver.find_element(By.NAME, 'csrf_token')
+        # print(registration_form.text)
+
         return f"""__cq_dnt={__cq_dnt}; dw_dnt={dw_dnt}; _gcl_au={_gcl_au}; _fbp={_fbp}; _gid={_gid}; _tt_enable_cookie={_tt_enable_cookie}; _ttp={_ttp}; _li_dcdm_c={_li_dcdm_c}; _lc2_fpi={_lc2_fpi}; __cq_uuid={__cq_uuid}; __cq_seg={__cq_seg}; dw={dw}; dw_cookies_accepted={dw_cookies_accepted}; _sp_ses.12ed={_sp_ses}; cquid={cquid}; _gat_UA-11214620-1={_gat_UA}; sid={sid}; dwsid={dwsid}; dwanonymous_14a9e20de599203e490e731dc5bac197={dwanonymous_14a9e20de599203e490e731dc5bac197}; dwac_c26e5001b1c5ffde452d7bcd56={dwac_c26e5001b1c5ffde452d7bcd56}; cqcid={cqcid}; _dc_gtm_UA-11214620-1={_dc_gtm_UA}; _ga={_ga}; _sp_id.12ed={_sp_id}; _ga_8MT9WZCNB1={_ga_A}; _ga_QKEQR920KZ={_ga_B};"""
 
 
@@ -113,15 +128,18 @@ class Generator:
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
         })
+   
 
         # print(page.cookies.get_dict())
         print(f'get page response... {page.status_code}')
         if page.status_code == 200:
             soup = BeautifulSoup(page.text,"html.parser")
-            self.csrf = soup.find("input",{"name":"csrf_token"})["value"]
+            self.registration_csrf = soup.find("input", {"name":"csrf_token"})['value']
             self.password_html = soup.find_all('input', {'type': 'password'})
             self.password_field = self.password_html[0]['id']
             self.confirm_password_field = self.password_html[1]['id']
+            self.captcha_token = self.__get_captcha()
+            self.cookie = self.get_cookie()
             self.create_account()
 
     def create_account(self):
@@ -135,15 +153,15 @@ class Generator:
             'dwfrm_profile_register_emailconfirm': f'{self.first}{self.last}123{script_config["catchall"]}',
             self.password_field: script_config["password"],
             self.confirm_password_field: script_config["password"],
-            'dwfrm_profile_register_phone': script_config["phone"],
             'dwfrm_profile_register_birthday': '05/06/2001',
             'dwfrm_profile_customer_gender': '1',
             'dwfrm_profile_customer_country': 'us',
             'dwfrm_profile_customer_postal': '18704',
-            'g-recaptcha-response': self.__get_captcha(),
+            'g-recaptcha-response': self.captcha_token,
             'dwfrm_profile_confirm': 'Apply',
-            'csrf_token': self.csrf
+            'csrf_token': self.registration_csrf
         }
+        
         createAcc = self.s.post(f'https://metallica.com/on/demandware.store/Sites-Metallica-Site/default/Account-RegistrationForm?&format=ajax',data=data,headers={
             'authority':'www.metallica.com',
             'path': '/on/demandware.store/Sites-Metallica-Site/Account-RegistrationForm',
@@ -153,7 +171,7 @@ class Generator:
             'accept-language':'en-US,en;q=0.9',
             'content-length': '1296',
             'content-type': 'application/x-www-form-urlencoded',
-            'cookie': self.get_cookie(),
+            'cookie': self.cookie,
             'origin': 'https://www.metallica.com',
             'referer': f'https://www.metallica.com/register',
             'sec-ch-ua': """sec-ch-ua: "Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24" """,
@@ -168,7 +186,7 @@ class Generator:
             'x-requested-with': 'XMLHttpRequest'
         })
     
-        print(f'create response... {createAcc.status_code}')
+        print(f'create response... {createAcc.json()}')
         self.email = f'{self.first}{self.last}123{script_config["catchall"]}:{script_config["password"]}'
         print(f'{self.first}{self.last}123{script_config["catchall"]}:{script_config["password"]}')
         with open('accounts.txt','a') as accountFile:
